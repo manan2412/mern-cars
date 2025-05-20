b6777793c55abf0df07e34740f77e07017fb7b15
Manan Patel <manan241214@gmail.com>
2025-05-20 19:29:12 +0530

    Completed signup page UI

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -13,6 +13,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json("User created successfully!");
   } catch (error) {
-    next(error);
+    f(error);
   }
 };
