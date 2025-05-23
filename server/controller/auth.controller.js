d66ff3a3ccb75d6258c04515e658818351c13e57
Manan Patel <manan241214@gmail.com>
2025-05-23 22:41:51 +0530

    Completed signin page functionality

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -29,7 +29,7 @@ export const signin = async (req, res, next) => {
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-    console.log(validUser);
+    // console.log(validUser);
     const { password: pass, ...rest } = validUser._doc;
     res
       .cookie("access_token", token, { httpOnly: true })
