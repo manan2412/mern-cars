8e153d115c1d58fc7f6136b644fa15adcd268175
Manan Patel <manan241214@gmail.com>
2025-06-11 21:31:16 +0530

    Created Create Listing API

---
+++ server/models/listing.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    noOfSeats: {
+      type: Number,
+      required: true,
+    },
+    noOfOwner: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    images: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+export default Listing;
