0f2a650aec4e3600f96346d58687319a948e1302
Manan Patel <manan241214@gmail.com>
2025-06-18 17:31:05 +0530

    Created get user list  api

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/errorHandler.js";
 import bcrypt from "bcryptjs";
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 export const test = (req, res) => {
   res.json({
@@ -39,9 +40,23 @@ export const deleteUser = async (req, res, next) => {
       throw errorHandler(401, "You can only delete your own account!");
     }
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted')
+    res.clearCookie("access_token");
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id == req.params.id) {
+    console.log("in getUser method");
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings"));
   }
 };
