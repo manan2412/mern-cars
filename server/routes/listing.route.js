d1f9fb4dbca9d1ec2fdf8703bc4f508b445da501
Manan Patel <manan241214@gmail.com>
2025-06-23 13:02:47 +0530

    Complete update user listing api

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing} from "../controller/listing.controller.js";
+import { createListing, deleteListing,updateListing} from "../controller/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;
