41be56bb47dc61fa1285e9c88f636dfada95146f
Manan Patel <manan241214@gmail.com>
2025-07-13 20:43:28 +0530

    Refactor About page layout and enhance server index.js for static file serving

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="py-20 px-20 max-w-xl mx-auto">
+    <div className="py-20 px-20 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4 text-slate-800">
         About Mern Cars
       </h1>
--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
-import bodyParser  from "body-parser";
-
+import bodyParser from "body-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -19,13 +19,14 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
 const app = express();
 
 // app.use(express.json());
 app.use(cookieParser());
 
-app.use(bodyParser.json({ limit: '20mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }));
 
 app.listen(3000, () => {
   console.log("Server Listening port 3000...");
@@ -34,6 +35,12 @@ app.listen(3000, () => {
 app.use("/server/user", userRouter);
 app.use("/server/auth", authRouter);
 app.use("/server/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
